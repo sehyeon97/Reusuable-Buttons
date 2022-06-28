03dc623aa35bcf9b83f9912938e837fa2475f555
sehyeon <sehyeonoh97@gmail.com>
2022-06-28 14:57:24 -0700

    added cursor and opacity attributes and changed type definitions to be more precise

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,23 @@
 import Style from './stylesheet/Style';
 import Button from './components/Button';
 
-const BoxShadowOnButton = {
-  boxShadow: '8px 16px red'
-}
-
-const BoxShadowOffButton = {
-  boxShadow: '0px 0px white'
-}
-
-const EnableButton = {
-  opacity: 1.0,
-  cursor: 'default'
-}
-
-const DisableButton = {
-  opacity: 0.6,
-  cursor: 'not-allowed'
-}
-
 function App() {
   return (
     <div style={Style.container}>
       <div style={Style.div}>
-        <Button variant="default" size="default" color='blue'/>
+        <Button variant="default" size="default" color='blue' cursor='not-allowed' opacity={1}/>
       </div>
       <div style={Style.div}>
-        <Button variant="outline" size="large" backgroundColor='orange'/>
+        <Button variant="outline" size="large" backgroundColor='orange' cursor='default' opacity={0.4}/>
       </div>
       <div style={Style.div}>
         <Button variant="text" size="small" color='red' backgroundColor='cornflowerblue'/>
       </div>
       <div style={Style.div}>
-        <Button boxshadow/>
+        <Button boxshadow='24px 24px blue'/>
       </div>
       <div style={Style.div}>
-        <Button size="default"/>
+        <Button size="default" boxshadow='24px 24px'/>
       </div>
     </div>
   );
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,27 +1,40 @@
+// Enable / disable button by using cursor and opacity
+
 interface Styles {
     variant?: string,
-    boxshadow?: boolean,
+    boxshadow?: string,
     size?: string,
     color?: string,
-    backgroundColor?: string
+    backgroundColor?: string,
+    cursor?: string,
+    opacity?: number
 }
 
 const Button = (attributes: Styles) => {
-    let size = attributes.size;
     let variant = attributes.variant;
+    let boxShadow = attributes.boxshadow;
+    let size = attributes.size;
     let color = attributes.color;
     let backgroundColor = attributes.backgroundColor;
+    let cursor = attributes.cursor;
+    let opacity = attributes.opacity;
+
+    const StyledVariant = getVariant(variant);
+
+    const StyledBoxShadow = getBoxShadow(boxShadow);
 
     const StyledSize = size ? getSize(size) : {
         width: '200px',
         height: '150px'
     }
 
-    const StyledVariant = getVariant(variant);
-
     const StyledColor = getColor(color);
 
     const StyledBackgroundColor = getBackgroundColor(backgroundColor);
+
+    const StyledCursor = getCursor(cursor);
+
+    const StyledOpacity = getOpacity(opacity);
 
     const StyledButton = {
         width: StyledSize.width,
@@ -29,7 +42,10 @@ const Button = (attributes: Styles) => {
         border: StyledVariant.border,
         borderRadius: StyledVariant.borderRadius,
         color: StyledColor.color,
-        backgroundColor: StyledBackgroundColor.backgroundColor
+        backgroundColor: StyledBackgroundColor.backgroundColor,
+        boxShadow: StyledBoxShadow.boxShadow,
+        cursor: StyledCursor.cursor,
+        opacity: StyledOpacity.opacity
     }
 
     return (
@@ -37,22 +53,7 @@ const Button = (attributes: Styles) => {
     );
 }
 
-const getSize = (size: string): React.CSSProperties => {
-    let returnSize;
-    switch (size) {
-        case "large":
-            returnSize = { width: '400px', height: '335px' }
-            break;
-        case "small":
-            returnSize = { width: '100px', height: '60px'}
-            break;
-        default:
-            returnSize = { width: '200px', height: '150px' }
-    }
-    return returnSize;
-}
-
-const getVariant = (variant: string | undefined): React.CSSProperties => {
+const getVariant = (variant: Styles["variant"]): React.CSSProperties => {
     let returnVariant;
     if (variant === undefined) {
         variant = "default";
@@ -86,18 +87,65 @@ const getVariant = (variant: string | undefined): React.CSSProperties => {
     return returnVariant;
 }
 
-const getColor = (color: string | undefined): React.CSSProperties => {
+// box shadow actually accepts 4 parts not just 3
+const getBoxShadow = (boxShadow: Styles["boxshadow"]): React.CSSProperties => {
+    if (boxShadow === undefined) {
+        return { boxShadow: '0px 0px white' }
+    }
+
+    let splitBoxShadow = boxShadow.split(' ');
+    if (!splitBoxShadow[2]) {
+        return { boxShadow: splitBoxShadow[0] + ' ' + splitBoxShadow[1] + ' black' }
+    }
+
+    return { boxShadow: splitBoxShadow[0] + ' ' + splitBoxShadow[1] + ' ' + splitBoxShadow[2] }
+}
+
+const getSize = (size: Styles["size"]): React.CSSProperties => {
+    let returnSize;
+    switch (size) {
+        case "large":
+            returnSize = { width: '400px', height: '335px' }
+            break;
+        case "small":
+            returnSize = { width: '100px', height: '60px'}
+            break;
+        default:
+            returnSize = { width: '200px', height: '150px' }
+    }
+    return returnSize;
+}
+
+const getColor = (color: Styles["color"]): React.CSSProperties => {
     if (color === undefined) {
         return { color: 'black' }
     }
+
     return { color: color }
 }
 
-const getBackgroundColor = (backgroundColor: string | undefined): React.CSSProperties => {
+const getBackgroundColor = (backgroundColor: Styles["backgroundColor"]): React.CSSProperties => {
     if (backgroundColor === undefined) {
         return { backgroundColor: 'white' }
     }
+
     return { backgroundColor: backgroundColor }
 }
+
+const getCursor = (cursor: Styles["cursor"]): React.CSSProperties => {
+    if (cursor === undefined || cursor !== 'not-allowed') {
+        return { cursor: 'default' }
+    }
+    return { cursor: 'not-allowed' }
+}
+
+const getOpacity = (opacity: Styles["opacity"]): React.CSSProperties => {
+    if (opacity === undefined) {
+        return { opacity: 1.0 }
+    }
+    return { opacity: opacity }
+}
+
+
 
 export default Button;
